98818c8a969dac156d1266e3e98433fcd8ccdbdd
TDavidson88 <troy_davidson@progressive.com>
2025-02-10 14:44:10 -0700

    day one commit

--- src/week1/TodosSolution/Todos.Api/Program.cs
+++ src/week1/TodosSolution/Todos.Api/Program.cs
@@ -1,39 +1,33 @@
-using Todos.Api.Todos;
+
 using Marten;
+using Todos.Api.Todos;
 
-namespace Todos.Api;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+builder.Services.AddAuthorization();
+
+var connectionString = builder.Configuration.GetConnectionString("todos") ??
+    throw new Exception("CAn't start the api without a connection string");
+builder.Services.AddMarten(builder =>
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    builder.Connection(connectionString);
+});
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+// above this line is configuration for the services inside our application
+var app = builder.Build();
+// after this line is configuration for how HTTP requests and responses and handled.
 
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        var connectionString = builder.Configuration.GetConnectionString("todos") ??
-        throw new Exception("CAn't start the api without a connection string");
-        builder.Services.AddMarten(builder =>
-        {
-            builder.Connection(connectionString);
-        });
-
-        app.UseAuthorization();
-
-        app.MapTodos();
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
 }
+
+app.UseAuthorization();
+app.MapTodos();
+
+app.Run(); // "Blocked" here. 
--- src/week1/TodosSolution/Todos.Api/Todos/Endpoints.cs
+++ src/week1/TodosSolution/Todos.Api/Todos/Endpoints.cs
@@ -1,23 +1,24 @@
 ﻿using Marten;
+
 namespace Todos.Api.Todos;
 
 public static class Endpoints
 {
-    // An "extension method"
-    // this 
+
+    // An "Extension Method"
+    // this will add a method to the Endpoint Route Builder called "MapTodos"
     public static IEndpointRouteBuilder MapTodos(this IEndpointRouteBuilder builder)
     {
-        //GET /todos
-        builder.MapGet("/todos", async (IDocumentSession session) => {
-     
+        // GET /todos
+        builder.MapGet("/todos", async (IDocumentSession session) =>
+        {
             var response = await session.Query<TodoListItem>().ToListAsync();
-
             return Results.Ok(response);
-
         });
+        // POST /todos
 
-        //POST /todos
-        builder.MapPost("/todos", (TodoListCreateItem request) =>
+
+        builder.MapPost("/todos", async (TodoListCreateItem request, IDocumentSession session) =>
         {
 
             var response = new TodoListItem
@@ -27,22 +28,25 @@ public static class Endpoints
                 Completed = false,
                 CreatedOn = DateTimeOffset.UtcNow
             };
+
+            session.Store(response);
+            await session.SaveChangesAsync();
             return Results.Ok(response);
         });
         return builder;
     }
-
 }
 
 public record TodoListItem
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; set; }
     public string Description { get; set; } = string.Empty;
+
     public bool Completed { get; set; }
-    public DateTimeOffset CreatedOn { get; init; }
+
+    public DateTimeOffset CreatedOn { get; set; }
     public DateTimeOffset? CompletedOn { get; set; }
 }
-
 
 public record TodoListCreateItem
 {
