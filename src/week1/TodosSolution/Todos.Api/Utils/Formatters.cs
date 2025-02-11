41f79438749930351e0a11a6cf740a58459f3540
TDavidson88 <troy_davidson@progressive.com>
2025-02-11 10:33:24 -0700

    before lunch

---
+++ src/week1/TodosSolution/Todos.Api/Utils/Formatters.cs
@@ -0,0 +1,11 @@
+﻿namespace Todos.Api.Utils;
+
+public class Formatters
+{
+    // Method that takes two strings and returns a string
+    public string FormatName(string firstName, string lastName)
+    {
+        // Return the formatted string
+        return $"{lastName}, {firstName}";
+    }
+}
---
+++ src/week1/TodosSolution/Todos.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Todos.Api.Utils;
+
+namespace Todos.Tests;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void CanFormatName()
+    {
+        //Given
+        string firstName = "John", lastName = "Doe", fullName;
+        var formatter = new Formatters();
+
+        //When
+        fullName = formatter.FormatName(firstName, lastName);
+
+        //Then
+        Assert.Equal("Doe, John", fullName);
+    }
+
+    [Theory]
+    [InlineData("Luke", "Skywalker", "Luke Skywalker")]
+    public void CanFormatNameWithDifferentNames(string firstName, string lastName, string expected)
+    {
+        //Given
+        var formatter = new Formatters();
+        //When
+        var fullName = formatter.FormatName(firstName, lastName);
+        //Then
+        Assert.Equal(expected, fullName);
+    }
+}
